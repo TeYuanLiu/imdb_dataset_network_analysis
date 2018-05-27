4c4fd464d56a43fb152e51ac07d2b1ef0b48bff1
wesleytzou <33106482+wesleytzou@users.noreply.github.com>
2018-05-26 17:44:12 -0700

    Drop everything after ( yyyy|???? /III)

--- project4_Merge.py
+++ project4_Merge.py
@@ -5,22 +5,19 @@ file_merge = open("merge_movies.txt", "r+",encoding='ISO-8859-1')
 file_actor = open("actor_movies.txt", "r",encoding='ISO-8859-1')
 file_actress = open("actress_movies.txt", "r",encoding='ISO-8859-1') 
 
-for i, line in enumerate(file_actress):
-    newline = line.split("\t\t")
-    for j in range(len(newline)):
-        print(newline[j])
-        newline[j] = re.sub(r"(?<=\(\d{4}\)).*$",'',str(newline[j]))
-        print(newline[j])
-    if len(newline) > 10:
-        file_merge.write(str(newline)+'\n')
-    if i >= 3:
-        break
-"""
-for i, line in enumerate(file_actor):
-    newline = line.split("\t\t")
-    if len(newline) > 10:
-        file_merge.write(str(newline)+'\n')
-"""
+def clean_merge(filename):
+    for i, line in enumerate(filename):
+        newline = line.split("\t\t")
+        for j in range(len(newline)):
+            pattern = re.search(r"\((\d{4}|\?{4})\/*I{0,}\)(.*)$", newline[j])
+            if pattern:
+                newline[j] = newline[j].replace(pattern.group(2),'')
+        if len(newline) > 10:
+            file_merge.write(str(newline)+'\n')
+
+clean_merge(file_actress)
+clean_merge(file_actor)
+
 file_actress.close()
 file_actor.close()
 file_merge.close()
