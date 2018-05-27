f785cb9f0400cd1a4938a44e2d3511db03b1fca4
wesleytzou <33106482+wesleytzou@users.noreply.github.com>
2018-05-27 14:14:38 -0700

    clean movie with less than 5 actor/actress

--- project4_Merge.py
+++ project4_Merge.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 import re
 
-def clean_merge(filename):
-    for i, line in enumerate(filename):
+def clean_merge(input_file, output_file):
+    for i, line in enumerate(input_file):
         newline = line.split("\t\t")
         if len(newline) <= 10:
             continue
@@ -15,28 +15,57 @@ def clean_merge(filename):
             strip_list.append(obj.strip("\n").strip())
         strip_list = list(filter(None, strip_list))
         string = ','.join([str(obj) for obj in strip_list])
-        file_merge.writelines(string+'\n')
+        output_file.writelines(string+'\n')
 """"
 file_merge = open("merge_test.txt", "w+",encoding='ISO-8859-1')
 file_actor = open("actor_movies.txt", "r",encoding='ISO-8859-1')
 file_actress = open("actress_movies.txt", "r",encoding='ISO-8859-1') 
 
-clean_merge(file_actress)
-clean_merge(file_actor)
+clean_merge(file_actress, file_merge)
+clean_merge(file_actor, file_merg)
 
 file_merge.close()
 file_actress.close()
 file_actor.close()
 """
 
+"""
+movie_list = open("movie_list.txt", "w+",encoding='ISO-8859-1')
 file_merge = open("merge_test.txt", "r",encoding='ISO-8859-1') 
-
+movies = {}
 for i, line in enumerate(file_merge):
     newline = line.rstrip('\n')
     newline = line.split(",")
-    for obj in newline:
-        print(obj.strip("\n").strip())
-    if i > 2:
-       break
-   
+    character = newline[0].strip("\n").strip()
+    for j in range(1,len(newline)):
+        movie_name = newline[j].strip("\n").strip()
+        if movie_name in movies:
+            movies[movie_name] += ","+character
+        else:
+            movies[movie_name] = character
+    #if i > 2000:
+       #break
+for key, value in movies.items():
+    movie_list.writelines(key+','+value+'\n')
 file_merge.close()
+movie_list.close()
+"""
+
+
+movie_list = open("movie_list.txt", "r",encoding='ISO-8859-1')
+movie_clean = open("movie_clean.txt", "w+",encoding='ISO-8859-1')
+
+for i, line in enumerate(movie_list):
+    newline = line.split(',')
+    if len(newline) <= 5:
+        continue
+    strip_list = []
+    for obj in newline:
+        strip_list.append(obj.strip("\n").strip())
+    strip_list = list(filter(None, strip_list))
+    string = ','.join([str(obj) for obj in strip_list])
+    movie_clean.writelines(string+'\n')
+
+movie_clean.close()
+movie_list.close()
+
