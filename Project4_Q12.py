484d6847e0c573dc6b97f9e2885c2b829be67e1e
Juo Wen <mandyfu84@gmail.com>
2018-06-03 13:39:38 -0700

    Q12

--- Project4_Q12.py
+++ Project4_Q12.py
@@ -8,10 +8,10 @@ import sklearn.metrics as mcs
 from collections import defaultdict
 import time
 
-fname = "movie_genre.txt"
+fname = "movie_rating.txt"
 
 start = time.time()
-movie_genre_dict = {}
+movie_rating_dict = {}
 line_count = 0
 
 with open(fname, encoding="ISO-8859-1") as fin:
@@ -24,14 +24,43 @@ with open(fname, encoding="ISO-8859-1") as fin:
         p1 = re.search(r"\((\d{4}|\?{4})[^()]*\)(.+)", obj)
         if p1:
             obj = obj.replace(p1.group(2),"")
-            movie_genre_dict[obj]=obj_list[1]
+            movie_rating_dict[obj]=obj_list[1]
         else:
             p2 = re.search(r"\((\d{4}|\?{4})[^()]*\)", obj)
             if not p2:
                 obj = obj + " (????)"
-            movie_genre_dict[obj]=obj_list[1]
+            movie_rating_dict[obj]=obj_list[1]
 
 
-with open("movie_genre_clean.txt", "w", encoding="utf-8") as outfile:
-    for k, v in movie_genre_dict.items():
+with open("movie_rating_clean.txt", "w", encoding="utf-8") as outfile:
+    for k, v in movie_rating_dict.items():
+        outfile.write("%s\t%s" %(k, v)) 
+
+
+fname = "director_movies.txt"
+
+start = time.time()
+movie_director_dict = {}
+line_count = 0
+
+with open(fname, encoding="ISO-8859-1") as fin:
+    for line in fin:
+        line_count += 1
+        obj_list = re.split(r"\t+", line) ############# change to \t        
+
+        obj = obj_list[0]
+
+        p1 = re.search(r"\((\d{4}|\?{4})[^()]*\)(.+)", obj)
+        if p1:
+            obj = obj.replace(p1.group(2),"")
+            movie_director_dict[obj_list[1]]=obj
+        else:
+            p2 = re.search(r"\((\d{4}|\?{4})[^()]*\)", obj)
+            if not p2:
+                obj = obj + " (????)"
+            movie_director_dict[obj_list[1]]=obj
+
+
+with open("movie_director_clean.txt", "w", encoding="utf-8") as outfile:
+    for k, v in movie_director_dict.items():
         outfile.write("%s\t%s" %(k, v)) 
